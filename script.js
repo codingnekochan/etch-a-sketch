416efcae5a37fa63fa2395bd0f2210a77cf17374
Mariam Ibrahim <ibrahimoyiza198@gmail.com>
2023-09-27 17:51:31 +0100

    refactor js file to solve slowing issues

--- script.js
+++ script.js
@@ -1,91 +1,103 @@
-const sketchInterface = document.querySelector('.sketch_surface');
-const gridSizeButton = document.querySelector('.ui_button--grid');
-// creating the grid according to user input
-gridSizeButton.addEventListener('click', ()=>{
-       userGridSize = getSize();
-       sketchInterface.innerHTML =''
-       createGrid(userGridSize);
-})
-// 
-const eraserButton = document.querySelector('.ui_button--ersr')
-const penColorButton = document.querySelector('.ui_button--colr');
-const colorPicker = document.querySelector('#color-picker');
-const defaultColor = document.querySelector('.ui_button--dflt');
-const gridLineButton = document.querySelector('.ui_button--lines');
-const clearButton = document.querySelector('.ui_button--clr');
-const rainbowColorButton = document.querySelector('.ui_button--rnbw')
-function createGrid(userGridSize){
-    for (let i = 1; i<= (userGridSize**2); i++){
-        const gridSquares = document.createElement('div');
-        sketchInterface.appendChild(gridSquares);
-        gridSquares.style.minWidth = `${450/ userGridSize}px`
-        gridSquares.style.minHeight= `${450/ userGridSize}px`
-        gridSquares.classList.add('grid_squares');
-        // button functions
-        function useDefaultColor(){ 
-            gridSquares.style.background='#000000'
-        }
-        function clearBoard(){
-            gridSquares.style.background='#ffffff'
-        } 
-        function draw(){
-            gridSquares.addEventListener('mouseover',()=>{
-                useDefaultColor();
-            })
-        }
-        function eraser(){
-            gridSquares.addEventListener('mouseover',()=>{
-                clearBoard();
-            })
-        }
-        function changeColor(){
-            gridSquares.addEventListener('mouseover',()=>{
-                gridSquares.style.background = `${colorPicker.value}`
-            })
-        } 
-        function addGridLines(){
-            gridSquares.classList.toggle('gridlines')
-        }
-        function useRainbowColor(){
-            gridSquares.addEventListener('mouseover',()=>{
-                gridSquares.style.background = generateColor();
-            })
-        }
-        // button events   
-        sketchInterface.addEventListener('dblclick',draw);
-        penColorButton.addEventListener('click', changeColor);
-        defaultColor.addEventListener('click',draw)
-        eraserButton.addEventListener('click', eraser);
-        clearButton.addEventListener('click', clearBoard);
-        gridLineButton.addEventListener('click', addGridLines);
-        rainbowColorButton.addEventListener('click', useRainbowColor);
-    }
+const sketchInterface = document.querySelector(".sketch_surface");
+const gridSizeButton = document.querySelector(".ui_button--grid");
+const eraserButton = document.querySelector(".ui_button--ersr");
+const penColorButton = document.querySelector(".ui_button--colr");
+const colorPicker = document.querySelector("#color-picker");
+const defaultColor = document.querySelector(".ui_button--dflt");
+const gridLineButton = document.querySelector(".ui_button--lines");
+const clearButton = document.querySelector(".ui_button--clr");
+const rainbowColorButton = document.querySelector(".ui_button--rnbw");
+
+// Button Events Listener
+gridSizeButton.addEventListener("click", handleButtonSize);
+sketchInterface.addEventListener("dblclick", useDefaultColor);
+penColorButton.addEventListener("click", changeColor);
+defaultColor.addEventListener("click", useDefaultColor);
+eraserButton.addEventListener("click", eraser);
+clearButton.addEventListener("click", clearBoard);
+gridLineButton.addEventListener("click", addGridLines);
+rainbowColorButton.addEventListener("click", useRainbowColor);
+
+function createGrid(userGridSize) {
+  sketchInterface.innerHTML = " ";
+  for (let i = 1; i < userGridSize ** 2; i++) {
+    let gridSquares = document.createElement("div");
+    gridSquares.classList.add("grid_squares");
+    gridSquares.style.minWidth = `${450 / userGridSize}px`;
+    gridSquares.style.minHeight = `${450 / userGridSize}px`;
+    sketchInterface.appendChild(gridSquares);
+  }
 }
-// default grid dimension
-createGrid(16)
-// get user input
-function getSize(){
-    let input = prompt ("Enter grid Size", '');
-    if (input < 16 || input > 100){
-        alert ("please enter a number between 16 and 100");
-        input = prompt("Enter grid Size", '');
-        return input;
-    }
-    else if (input === ""){
-        alert("please enter a number");
-       input = prompt("Enter grid size", '');
-       return input;
-    }
-    else{
-        return input;
-    }
+createGrid(16);
+
+function handleButtonSize() {
+  let userGridSize = getSize();
+  createGrid(userGridSize);
+  console.log("Grid size picked!");
 }
-// get random color
-function generateColor(){
-    let colorR = Math.floor(Math.random()* 256);
-    let colorG = Math.floor(Math.random()* 256);
-    let colorB = Math.floor(Math.random()* 256);
-    let rainbowColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
-    return rainbowColor;
- 
+
+function useDefaultColor() {
+  let gridSquares = sketchInterface.querySelectorAll('.grid_squares')
+  gridSquares.forEach((gridSquare)=>{
+    gridSquare.addEventListener('mouseover',()=>{
+      gridSquare.style.background = '#000000';
+    })
+  })
+}
+function clearBoard() {
+  let gridSquares = sketchInterface.querySelectorAll('.grid_squares')
+  gridSquares.forEach((gridSquare)=>{
+      gridSquare.style.background = '#ffffff';
+  })
+}
+function eraser() {
+  let gridSquares = sketchInterface.querySelectorAll('.grid_squares')
+  gridSquares.forEach((gridSquare)=>{
+    gridSquare.addEventListener('mouseover',()=>{
+      gridSquare.style.background = '#ffffff';
+    })
+  })
+}
+function changeColor() {
+  let gridSquares = sketchInterface.querySelectorAll('.grid_squares')
+  gridSquares.forEach((gridSquare)=>{
+    gridSquare.addEventListener('mouseover',()=>{
+      gridSquare.style.background = `${colorPicker.value}`
+    })
+  })
+}
+function addGridLines() {
+  const gridSquares = sketchInterface.querySelectorAll(".grid_squares");
+  gridSquares.forEach((box) => {
+    box.classList.toggle("gridlines");
+  });
+  console.log("Grid line added!");
+}
+function useRainbowColor() {
+  let gridSquares = sketchInterface.querySelectorAll('.grid_squares')
+  gridSquares.forEach((gridSquare)=>{
+    gridSquare.addEventListener('mouseover',()=>{
+      gridSquare.style.background = generateColor();
+    })
+  })
+}
+
+function getSize() {
+  let input = prompt("Enter grid Size", "");
+  input = parseInt(input); // convert the inputed value to integer
+
+  // Include a conditional incase a user inputs in a string
+  if (isNaN(input) || input < 16 || input > 100) {
+    alert("please enter a number between 16 and 100");
+    return getSize(); // Automatically returns the function if this condition is not met
+  }
+  return input;
+}
+function generateColor() {
+  let colorR, colorG, colorB;
+
+  colorR = Math.floor(Math.random() * 256);
+  colorG = Math.floor(Math.random() * 256);
+  colorB = Math.floor(Math.random() * 256);
+  return `rgb(${colorR}, ${colorG}, ${colorB})`;
 }
